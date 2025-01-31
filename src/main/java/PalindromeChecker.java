59be7301eef68736e39669e9f406a78ac3ed2f6e
demei1 <devinmei6@gmail.com>
2025-01-31 11:37:53 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -35,15 +35,43 @@ public void tester()
     }
   }
 }
+public void setup()
+{
+  String lines[] = {"test", "rotator", "rewriter", "nurses run", "Madam, I'm Adam!", "A Man! A Plan! A Canal! Panama!"};
+  System.out.println("there are " + lines.length + " lines");
+  for (int i=0; i < lines.length; i++) 
+  {
+    if(palindrome(lines[i])==true)
+    {
+      System.out.println(lines[i] + " IS a palindrome.");
+    }
+    else
+    {
+      System.out.println(lines[i] + " is NOT a palindrome.");
+    }
+  }
+}
 public boolean palindrome(String word)
 {
-  //your code here
-  return false;
+  String sWord = new String();
+  for (int i = 0; i < word.length(); i++){
+    if ( Character.isLetter( word.charAt(i) ) ) {
+      sWord += word.substring(i,i+1).toLowerCase();
+    }
+  }
+  
+  for (int i = 0; i < sWord.length(); i++){
+    if (!sWord.substring(i, i+1).equals(sWord.substring(sWord.length()-1 - i,sWord.length() - i)) ){
+      return false;
+    }
+  }
+  return true;
 }
 public String reverse(String str)
 {
-    String sNew = new String();
-    //your code here
-    return sNew;
-}
+  String result = "";
+  for(int i = str.length()-1; i >= 0 ; i--){
+    result += str.substring(i,i+1);
+  }
+  return result;
 }
